17b0a8387594856a8d89a4c1f285db8487e43685
suchit-simform <suchit.sheth@simformsolutions.com>
2025-09-03 17:12:57 +0530

    feat: add username support inside navbar

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,3 +1,5 @@
+import type { User } from "src/types/user";
+
 export const toCapitalize = (str: string) => {
   return str.charAt(0).toUpperCase() + str.slice(1);
 };
@@ -7,4 +9,13 @@ export function generateAuthToken() {
   const array = new Uint8Array(32);
   window.crypto.getRandomValues(array);
   return Array.from(array, (b) => b.toString(16).padStart(2, "0")).join("");
+}
+
+export function getUserName(user: User | null | undefined) {
+  if (!user) {
+    return "Guest";
+  }
+  if (!user?.email || !user?.role) return "Guest";
+
+  return user.email.split("@")[0].substring(0, 2).toLocaleUpperCase() || "";
 }
