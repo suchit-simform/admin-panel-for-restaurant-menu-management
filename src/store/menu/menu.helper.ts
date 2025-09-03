867e2399a4251913c5dae42557576a7fb60d25dd
suchit-simform <suchit.sheth@simformsolutions.com>
2025-09-03 16:29:19 +0530

    refa: improve common get stored menu items util function

--- src/store/menu/menu.helper.ts
+++ src/store/menu/menu.helper.ts
@@ -3,6 +3,14 @@ import type { Menu } from "./menu.type";
 
 export const getStoredMenuItems = () => {
   const stored = localStorage.getItem(LOCAL_STORAGE_MENU_KEY);
-  const menuItems: Menu[] = stored ? JSON.parse(stored) : [];
+  let menuItems: Menu[] = [];
+  if (stored) {
+    try {
+      menuItems = JSON.parse(stored);
+    } catch (err) {
+      console.log(`Exception while parsing localstorage menu item data: ${err}`);
+      menuItems = [];
+    }
+  }
   return menuItems;
 };
