3d4414265aad02188f2a3ab8cc2089463540a151
suchit-simform <suchit.sheth@simformsolutions.com>
2025-09-03 20:20:17 +0530

    style: improve antd style with antd theme editor

--- src/theme/themeAntd.ts
+++ src/theme/themeAntd.ts
@@ -1,13 +1,14 @@
 import type { ThemeConfig } from "antd";
 
+/**
+ * @ref: https://ant.design/theme-editor#Style
+ * ? Update theme config with help of above them editor by antd
+ */
 const theme: ThemeConfig = {
   token: {
-    // Seed Token
-    // colorPrimary: "#040b6b",
+    colorPrimary: "#ea495c",
+    colorInfo: "#ea495c",
     borderRadius: 4,
-
-    // Alias Token
-    // colorBgContainer: "#f6ffed",
   },
 };
 
