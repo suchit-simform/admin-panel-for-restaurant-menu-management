d681657200153d8f314ceeb5b54f946d6b514ba4
suchit-simform <suchit.sheth@simformsolutions.com>
2025-09-04 19:24:56 +0530

    Feat/menu add and edit operation (#7)
    * wip: menu item list with mock data
    
    * feat: add delete button with popconfirm
    
    * style: improve antd style with antd theme editor
    
    * feat: improve index, add and edit menu page
    
    - add MenuDataTable component
    - add menu add page
    - add menu edit page
    - improve menu index page
    
    :feat/add-menu-item-list-data-table|suchit-simform
    
    * impr: menu module with requested feedback changes
    
    * feat: add edit, and common header layout component, improve delete button
    
    - add icon and text support for edit and delete button
    - add header common layout dynamic component for crud operation
    
    :feat/add-menu-item-list-data-table|suchit-simform
    
    * feat: create common util function for currency format
    
    * impr: MenuDataTable component and Menu Types
    
    * refa: consume header component inside menu pages
    
    * refa: change consume pattern of event handler
    
    * refa: improve currency util function and route syntax error
    
    * style: add card case design inside menu list page
    
    * feat: add common menu form for add and edit operation of menu module
    
    * chor: ignore scannerwork folder

--- src/pages/menu/MenuEdit.tsx
+++ src/pages/menu/MenuEdit.tsx
@@ -1,26 +1,50 @@
-import { Flex } from "antd";
+import { Button, Card, Flex, Form } from "antd";
 import { useParams } from "react-router-dom";
 import Header from "../../components/organism/Header";
+import MenuForm from "./components/MenuForm";
 
 const MenuEdit = () => {
+  /**
+   * TODO:
+   * [] fetch menu item details using params.menuId
+   * [] fetch category list and show in select
+   * [] fetch ingredients list and show in select
+   * [] on form submit send data to backend
+   */
   const params = useParams<{ menuId?: string }>();
 
   return (
     <Flex vertical>
-      <Header
-        headerType="edit"
-        moduleName="menu"
-        moduleRouteKey="menu"
-        onAddClick={() => {
-          console.log("Add button clicked");
-        }}
-        title="Menu Feature"
-      />
-      <div>
-        <ul>
-          <li>Menu Item ID: {params.menuId}</li>
-        </ul>
-      </div>
+      <Card
+        title={
+          <Header
+            headerType="edit"
+            moduleName="menu"
+            moduleRouteKey="menu"
+            onAddClick={() => {
+              console.log("Add button clicked");
+            }}
+            title="Menu Feature"
+          />
+        }
+      >
+        <Flex gap={8} wrap style={{ paddingInline: 8, paddingBlock: 16 }}>
+          <h2>Menu Item ID: {params.menuId}</h2>
+          <Form
+            name="menu_add"
+            scrollToFirstError
+            variant="outlined"
+            size="middle"
+            layout="vertical"
+            style={{ width: "100%" }}
+          >
+            <MenuForm />
+            <Button type="primary" htmlType="submit" style={{ width: "fit-content" }}>
+              Save Changes
+            </Button>
+          </Form>
+        </Flex>
+      </Card>
     </Flex>
   );
 };
